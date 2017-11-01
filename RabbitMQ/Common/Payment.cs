0a77f72c891d44b020060b73d3a8c9e335663eda
David Lopera <dcgtq@outlook.com>
2017-11-01 10:06:48 -0500

    Commit proyectos

---
+++ RabbitMQ/Common/Payment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQ.Commom
+{
+  [Serializable]
+  public class Payment
+  {
+    public decimal AmountToPay;
+    public string CardNumber;
+    public string Name;
+  }
+}
